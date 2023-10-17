1bbb781027d5635e82e02be96bf81d0bc5b0eb76
Gabriel Bezerra <gabriel.beta85@gmail.com>
2023-10-17 16:33:44 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/gabrielbezerra/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbezerra.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
