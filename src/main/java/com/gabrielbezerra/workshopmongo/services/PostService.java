128cb0cdebb83e1f75e792d2a5cf5f04e475050a
Gabriel Bezerra <gabriel.beta85@gmail.com>
2023-10-26 10:47:47 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gabrielbezerra/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.gabrielbezerra.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielbezerra.workshopmongo.domain.Post;
+import com.gabrielbezerra.workshopmongo.repository.PostRepository;
+import com.gabrielbezerra.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id){
+		
+		Optional<Post> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	
+	}
+	
+}
