4990339f1071ef899ae8534ab975c77ceaa3519e
Gabriel Bezerra <gabriel.beta85@gmail.com>
2023-10-26 10:33:17 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/gabrielbezerra/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielbezerra/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gabrielbezerra.workshopmongo.domain.Post;
 import com.gabrielbezerra.workshopmongo.domain.User;
 import com.gabrielbezerra.workshopmongo.dto.UserDTO;
 import com.gabrielbezerra.workshopmongo.services.UserService;
@@ -67,4 +68,11 @@ public class UserResource {
 	
 	}
 
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
